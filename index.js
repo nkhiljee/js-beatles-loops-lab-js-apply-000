8041bacf7159a9e39d50c3a398a237bee0e8c36d
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-07 02:22:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var empty = [];
+  for (let i = 0; i < musicians.length; i++) {
+    empty.push(musicians[i] + ` plays ` + instruments[i])
+ }
+  return empty
+}
+
+function johnLennonFacts(facts) {
+  let newFacts = [];
+  let i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + '!!!')   
+    i++;
+  }
+  return newFacts
+}
+
+function iLoveTheBeatles(num) {
+  let empty = [];
+  do {
+    empty.push("I love the Beatles!")
+    num++;
+  }
+  while (num < 15)
+  
+  return empty
+}
